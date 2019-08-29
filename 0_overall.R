ab9eaaa05125149552ad41a34571618b5c93eea2
pmerkx <41674175+pmerkx@users.noreply.github.com>
2019-08-29 16:43:33 +0200

    update

--- 0_overall.R
+++ 0_overall.R
@@ -4,7 +4,7 @@
 ### 1 - reading and cleaning ###
 
 ## Read csv-files, do some basic formatting and save as Rda-file
-#source("Scripts/1a_reading_portcalls.R", echo=TRUE)
+source("Scripts/1a_reading_portcalls.R", echo=TRUE)
 
 ## Clean up the raw Rda-file
 source("Scripts/1b_cleaning_portcalls.R", echo=TRUE)
@@ -12,11 +12,17 @@ source("Scripts/1b_cleaning_portcalls.R", echo=TRUE)
 ## Pre-process data for analysis
 source("Scripts/1c_preproc_portcalls.R", echo=TRUE)
 
+## Get AIS data of specific ships within a specific time range
+source("Scripts/1d_reading_ais.R", echo=TRUE)
+
 
 ### 2 - analysis ###
 
-## Exploratory
-#rmarkdown::render("Scripts/2_analysis_portcalls.Rmd")
+## Exploratory analysis
+rmarkdown::render("Scripts/2_analysis_portcalls.Rmd")
+
+## Show tracks on the map
+source("Scripts/2_routes.R", echo=TRUE)
 
 
 ### 3 - presentation ###
