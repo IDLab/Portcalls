75efa7c0151a2134dd276a30854523779d93fb27
pmerkx <41674175+pmerkx@users.noreply.github.com>
2019-08-29 18:28:49 +0200

    update

--- 2_routes.R
+++ 2_routes.R
@@ -4,6 +4,22 @@ library(leaflet)
 
 load("Data/1_reading_cleaning/AIS_selectie.Rda")
 
+# Selecteer IMO-nummer om de route van te bekijken; de bovenste IMO-nrs zijn de 'mooiste' routes
+AIS_selectie <-
+  AIS_selectie %>%
+  filter(t_imo == 9216470
+                  #9395367
+                  #9404364
+                  #9753674
+                  #9114244
+                  #9402677
+                  #9518804
+                  #9400356
+                  #9439151
+                  #8920581
+                  #9122112
+  )
+         
 ui <- fluidPage(
   sliderInput(inputId = "t_updatetime", label = "Time",
               min = min(AIS_selectie$t_updatetime), 
@@ -11,32 +27,32 @@ ui <- fluidPage(
               value = min(AIS_selectie$t_updatetime),
               step=3600,
               animate = T),
-  selectInput(inputId = "t_imo",
-              "Kies een IMO-nummer:",
-              choices = sort(unique(AIS_selectie$t_imo))),
   leafletOutput("AIS_map")
 )
 
-
 server <- function(input, output, session) {
   points <- reactive({
-    AIS_selectie %>% filter(t_updatetime == input$t_updatetime) %>% filter(t_imo == input$t_imo)
+    AIS_selectie %>% filter(t_updatetime == input$t_updatetime)
   })
   
   history <- reactive({
-    AIS_selectie %>% filter(t_updatetime <= input$t_updatetime) #%>% filter(t_imo == input$t_imo)
+    AIS_selectie %>% filter(t_updatetime <= input$t_updatetime)
   })
   
   output$AIS_map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      addMarkers(lng = ~t_longitude,
-                 lat = ~t_latitude,
-                 data = points()) %>%
-      addMarkers(lng = ~t_longitude,
-                 lat = ~t_latitude,
-                 data = history())
+      addCircleMarkers(lng = ~t_longitude,
+                       lat = ~t_latitude,
+                       data = points(),
+                       radius = 3) %>%
+      addCircleMarkers(lng = ~t_longitude,
+                       lat = ~t_latitude,
+                       data = history(),
+                       radius = 3,
+                       label = ~t_updatetime)
   })
 }
 
 shinyApp(ui, server)
+
